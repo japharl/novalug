b3de75b73c9e3aa0a92eb621b96a0d9abf8c0171
Zak Zebrowski <zak.zebrowski@gmail.com>
2019-05-27 15:02:24 -0400

    Update test_website.sh

--- test_website.sh
+++ test_website.sh
@@ -1,8 +1,10 @@
 # assumes build-essential is installed
 # creates a file server running on port 8000 of localhost
-zaz@zaz-linux:~$ nohup python3 -m http.server &
-# ssh over to serveo.net, exposing port 8000, via port 80
+nohup python3 -m http.server &
 
-zaz@zaz-linux:~$ ssh -R 80:localhost:8000 serveo.net
-Forwarding HTTP traffic from https://form.serveo.net
-Press g to start a GUI session and ctrl-c to quit.
+# ssh over to serveo.net, exposing port 8000, via port 80
+ssh -R 80:localhost:8000 serveo.net
+#Forwarding HTTP traffic from https://form.serveo.net
+#Press g to start a GUI session and ctrl-c to quit.
+
+# note - form (above) is a random subdomain asigned by serveo.  See http://serveo.net for more options.
